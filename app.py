4a5bdf863fcc3b45418e6e27e3e9817fa511301c
Didisimmons <onyema.onyejekwe@gmail.com>
2022-05-20 12:30:07 +0000

    add css file to static folder

--- app.py
+++ app.py
@@ -15,3 +15,9 @@ mongo = PyMongo(app)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
